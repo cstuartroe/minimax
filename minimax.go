7e16398119386c65c47a4937dd91ca64e6ccbd62
Conor Stuart-Roe <conorstuartroe@gmail.com>
2023-03-20 22:30:41 -0400

    Assistance

--- minimax.go
+++ minimax.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"math/rand"
+
 	"github.com/cstuartroe/minimax/gameplay"
 	"github.com/cstuartroe/minimax/mancala"
 	"github.com/cstuartroe/minimax/minimaxer"
@@ -9,10 +11,19 @@ import (
 func main() {
 	game := mancala.MancalaGame(6, 4)
 
+	mx1 := minimaxer.NewMinimaxer(game, 4)
+	mx2 := minimaxer.NewMinimaxer(game, 8)
+	me := gameplay.NewHumanPlayer("Conor", game)
+
 	var player1, player2 gameplay.Player[mancala.MancalaState]
 
-	player1 = gameplay.NewHumanPlayer("Conor", game)
-	player2 = minimaxer.NewMinimaxer(game, 8)
+	if rand.Float32() > .5 {
+		player1 = mx1
+		player2 = minimaxer.NewAssistedHumanPlayer(me, mx2)
+	} else {
+		player1 = minimaxer.NewAssistedHumanPlayer(me, mx1)
+		player2 = mx2
+	}
 
 	gp := gameplay.NewGameplay(game, player1, player2)
 
--- minimaxer/minimaxer.go
+++ minimaxer/minimaxer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 
+	"github.com/cstuartroe/minimax/gameplay"
 	"github.com/cstuartroe/minimax/games"
 )
 
@@ -34,8 +35,35 @@ func (m *Minimaxer[State]) ChooseMove(prospect games.Prospect[State]) games.Move
 	return *move
 }
 
+type RatedMove[State games.GameState] struct {
+	Move  games.Move[State]
+	Score int
+}
+
+func (m *Minimaxer[State]) RateChoices(prospect games.Prospect[State]) []RatedMove[State] {
+	m.prospectScores = map[string]int{}
+	out := []RatedMove[State]{}
+
+	for _, move := range m.game.Describe(prospect).Moves {
+		out = append(out, RatedMove[State]{
+			Score: m.getProspectScore(moveToProspect(move, prospect.FirstAgent), m.lookahead),
+			Move:  move,
+		})
+	}
+
+	return out
+}
+
 func (m Minimaxer[State]) Comment() string {
 	return fmt.Sprintf("I analyzed %d game states!", len(m.prospectScores))
+}
+
+func moveToProspect[State games.GameState](move games.Move[State], firstAgent bool) games.Prospect[State] {
+	if !move.RetainControl {
+		firstAgent = !firstAgent
+	}
+
+	return games.Prospect[State]{State: move.State, FirstAgent: firstAgent}
 }
 
 func (m *Minimaxer[State]) chooseMove(prospect games.Prospect[State], searchDepth int) (int, *games.Move[State]) {
@@ -49,12 +77,7 @@ func (m *Minimaxer[State]) chooseMove(prospect games.Prospect[State], searchDept
 	var goodMoves []games.Move[State]
 
 	for i, move := range sd.Moves {
-		firstAgent := !prospect.FirstAgent
-		if move.RetainControl {
-			firstAgent = prospect.FirstAgent
-		}
-
-		ps := m.getProspectScore(games.Prospect[State]{State: move.State, FirstAgent: firstAgent}, searchDepth-1)
+		ps := m.getProspectScore(moveToProspect(move, prospect.FirstAgent), searchDepth-1)
 		if (i == 0) || (ps > score && prospect.FirstAgent) || (ps < score && !prospect.FirstAgent) {
 			score = ps
 			goodMoves = []games.Move[State]{move}
@@ -76,4 +99,69 @@ func (m *Minimaxer[State]) getProspectScore(prospect games.Prospect[State], sear
 	}
 
 	return m.prospectScores[scoreString]
+}
+
+type AssistedHumanPlayer[State games.GameState] struct {
+	human     gameplay.HumanPlayer[State]
+	minimaxer *Minimaxer[State]
+}
+
+func NewAssistedHumanPlayer[State games.GameState](human gameplay.HumanPlayer[State], minimaxer *Minimaxer[State]) AssistedHumanPlayer[State] {
+	return AssistedHumanPlayer[State]{human, minimaxer}
+}
+
+func (p AssistedHumanPlayer[State]) Name() string {
+	return p.human.Name() + " with some help from " + p.minimaxer.Name()
+}
+
+func (p AssistedHumanPlayer[State]) ChooseMove(prospect games.Prospect[State]) games.Move[State] {
+	move := p.human.ChooseMove(prospect)
+
+	bestScore := -100000
+	bestMoves := []games.Move[State]{}
+
+	fmt.Println("Good thought! Here's how the minimaxer rates the moves:")
+	for _, ratedMove := range p.minimaxer.RateChoices(prospect) {
+		fmt.Printf("%s: %d\n", ratedMove.Move.Summary, ratedMove.Score)
+		score := ratedMove.Score
+		if !prospect.FirstAgent {
+			score = -ratedMove.Score
+		}
+		if score > bestScore {
+			bestScore = score
+			bestMoves = []games.Move[State]{ratedMove.Move}
+		} else if score == bestScore {
+			bestMoves = append(bestMoves, ratedMove.Move)
+		}
+	}
+
+	recommendation := ""
+	agreed := false
+	for i, bestMove := range bestMoves {
+		if i > 0 {
+			recommendation += " or "
+		}
+		recommendation += bestMove.Summary
+		agreed = agreed || bestMove.Summary == move.Summary
+	}
+	fmt.Printf("%s recommends %s\n", p.minimaxer.Name(), recommendation)
+	if agreed {
+		fmt.Println("Looks like you two agree!")
+	}
+
+	fmt.Print("Change choice? ")
+	var answer string
+	_, err := fmt.Scanln(&answer)
+	if err != nil && err.Error() != "unexpected newline" {
+		panic(err)
+	}
+	if answer != "" && (answer[0] == 'y' || answer[0] == 'Y') {
+		move = p.human.ChooseMove(prospect)
+	}
+
+	return move
+}
+
+func (p AssistedHumanPlayer[State]) Comment() string {
+	return fmt.Sprintf("%s says %q, %s says %q", p.human.Name(), p.human.Comment(), p.minimaxer.Name(), p.minimaxer.Comment())
 }
